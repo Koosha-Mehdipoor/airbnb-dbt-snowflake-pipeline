2e284eb1cddb30fa25e0a482b93605b30e6be3b8
koosha.mehdipoor <koosha.mehdipoor@gmail.com>
2025-11-17 16:52:21 +0100

    creating a view upon the temp tables

---
+++ airbnb/models/staging/V_T_REVIEWS.sql
@@ -0,0 +1,14 @@
+WITH STG_REVIEW AS (
+
+SELECT * FROM {{ref('T_REVIEWS')}}
+
+)
+
+
+SELECT 
+    LISTING_ID,
+    REVIEW_DATE,
+    REVIEW_TEXT,
+    --removing null values from the sentiment column
+    COALESCE(REVIEW_SENTIMENT,'NA') AS REVIEW_SENTIMENT
+FROM STG_REVIEW
