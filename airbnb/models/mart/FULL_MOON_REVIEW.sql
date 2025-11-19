cb065df9833e8087f0dde81c4292eb6d28888aa0
koosha.mehdipoor <koosha.mehdipoor@gmail.com>
2025-11-19 17:26:08 +0100

    adding a seed and created a table based on the seed

---
+++ airbnb/models/mart/FULL_MOON_REVIEW.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized = 'table'
+    )
+    
+    }}
+
+
+WITH reviews AS (
+    SELECT * FROM {{ref('STG_REVIEWS')}}
+),
+full_moon_dates AS (
+    SELECT *
+    FROM {{ref('seed_full_moon_dates')}}
+)
+SELECT reviews.*, full_moon_dates.full_moon_date,
+        CASE WHEN full_moon_date IS NOT NULL THEN 'FULL MOON' ELSE 'NOT FULL MOON' END AS IS_FULL_MOON_REVIEW
+FROM reviews
+LEFT JOIN full_moon_dates
+ON TO_DATE(reviews.REVIEW_DATE) = TO_DATE(DATEADD(day, 1, full_moon_dates.FULL_MOON_DATE))
