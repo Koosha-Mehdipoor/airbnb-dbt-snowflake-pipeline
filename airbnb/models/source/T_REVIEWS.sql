1a96612a656acdaa23e7c027bd937e3ddd208e4d
koosha.mehdipoor <koosha.mehdipoor@gmail.com>
2025-11-19 16:32:02 +0100

    adding missing column to the model

--- airbnb/models/source/T_REVIEWS.sql
+++ airbnb/models/source/T_REVIEWS.sql
@@ -6,6 +6,7 @@ WITH RAW_REVIEWS AS (
 SELECT 
     listing_id,
     DATE AS review_date,
+    REVIEWER_NAME AS REVIEWER_NAME,
     comments AS review_text,
     sentiment AS review_sentiment
 FROM RAW_REVIEWS
