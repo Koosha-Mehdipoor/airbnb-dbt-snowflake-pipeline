b9af807555f431a92cc004225c3d9d0c8665939b
koosha.mehdipoor <koosha.mehdipoor@gmail.com>
2025-11-19 16:15:26 +0100

    Creating an incremental table for the reviews with a logic which is not very common in the real world

---
+++ airbnb/models/staging/STG_REVIEWS.sql
@@ -0,0 +1,25 @@
+
+{{
+  config(
+    materialized = 'incremental',
+    on_schema_change='fail',
+    cluster_by = ['REVIEW_DATE'],  
+    incremental_strategy = 'merge'
+  )
+}}
+
+WITH R AS (
+    SELECT * FROM {{ref('V_T_REVIEWS')}}
+)
+SELECT 
+    LISTING_ID,
+    REVIEW_DATE,
+    REVIEW_TEXT,
+    REVIEW_SENTIMENT
+FROM R
+
+{% if is_incremental() %}
+
+WHERE REVIEW_DATE > (SELECT COALESCE(MAX(REVIEW_DATE), '1900-01-01') FROM {{ this }})
+
+{%endif%}
